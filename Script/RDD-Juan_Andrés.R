32306ec31356db76353b9d69fb7df334fea7ec2e
Juan Andrés Rincón <ja.rincong@uniandes.edu.co>
2020-06-13 18:41:53 -0500

    Complete Script and Estimations

--- Script/RDD-Juan_Andrés.R
+++ Script/RDD-Juan_Andrés.R
@@ -17,7 +17,8 @@ ja.rincong@uniandes.edu.co"
   setwd(Juancho)
   
   #Packages
-  packs <- c("tidyverse","doBy","gdata","ggforce","haven","Hmisc","lubridate","rdd","readxl","sandwich","stargazer","dagitty","rdrobust","lmtest")
+  packs <- c("tidyverse","doBy","gdata","ggforce","ggpubr","haven","Hmisc","lubridate","rdd","readxl",
+             "sandwich","stargazer","dagitty","rdrobust","lmtest")
   sapply(packs,require,character=TRUE)
 
 "Data"
@@ -111,46 +112,217 @@ ja.rincong@uniandes.edu.co"
   
     
   plot(RDestimate(formula = male ~ bac1, data = dt, cutpoint = 0.08, bw = 0.05, kernel = "rectangular", se.type = "HC1"))
-  abline(v=0.08, lty = 2)  
+  abline(v=0.08, lty = 2)
   
-  ggplot(data = dt, mapping = aes(x = bac1, y = aged, color = DUI, group = DUI)) +
-    geom_point() + stat_smooth(size = 0.5, method = loess, formula = y ~ poly(x,2)) + stat_smooth(size = 0.5, method = lm)
-    geom_vline(xintercept = 0.08, linetype = "longdash") +
-    theme_classic()
+"------------------------------Covariate Balance Graphs------------------------------"
+
+  #Pretty Plots
+  bin <- cut(dt$bac1, seq(min(dt$bac1),max(dt$bac1),0.002)) #bins = 0.002 as in the paper
+  data_bin <- aggregate(dt,list(bin),function(x) { return(c(mean(x),length(x)))})
   
+  #Linear Fit
+    #Accident
+    ggacc <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=acc[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = acc, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm)+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(0.05,0.25)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control Fit","Treatment Fit"))+
+      labs(title = "Panel A. Accident at scene")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+    #Male
+    ggmale <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=male[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = male, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm)+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(0.74,0.82)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control Fit","Treatment Fit"))+
+      labs(title = "Panel B. Male")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+    #Age
+    ggage <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=aged[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = aged, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm)+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(33,39)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control Fit","Treatment Fit"))+
+      labs(title = "Panel C. Age")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+    #White
+    ggwhite <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=white[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = white, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm)+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(0.8,0.9)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control Fit","Treatment Fit"))+
+      labs(title = "Panel D. White")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+  #Linear fit group
+    ggarrange(ggacc,ggmale,ggage,ggwhite,ncol = 2, nrow = 2)
+    ggsave("Figures/CovBalanceLinear.pdf", height = 6, width = 9)
+    
+  #Quadratic Fit
+    #Accident
+    ggacc <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=acc[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = acc, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm, formula = y ~ poly(x,2))+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(0.05,0.25)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control QFit","Treatment QFit"))+
+      labs(title = "Panel A. Accident at scene")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+    #Male
+    ggmale <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=male[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = male, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm, formula = y ~ poly(x,2))+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(0.74,0.82)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control QFit","Treatment QFit"))+
+      labs(title = "Panel B. Male")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+    #Age
+    ggage <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=aged[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = aged, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm, formula = y ~ poly(x,2))+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(33,39)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control QFit","Treatment QFit"))+
+      labs(title = "Panel C. Age")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+    #White
+    ggwhite <- ggplot() + 
+      geom_point(data = data_bin, aes(x=bac1[,1],y=white[,1], alpha=.5)) + 
+      stat_smooth(data = dt, aes(x = bac1, y = white, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm, formula = y ~ poly(x,2))+
+      geom_vline(xintercept = 0.08, linetype = "longdash")+
+      scale_x_continuous(name = "BAC", limits = c(0,0.2))+
+      scale_y_continuous(name = "")+
+      coord_cartesian(ylim=c(0.8,0.9)) +
+      scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+      scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control QFit","Treatment QFit"))+
+      labs(title = "Panel D. White")+
+      theme_classic()+
+      theme(legend.position = "bottom")
+    #Linear fit group
+    ggarrange(ggacc,ggmale,ggage,ggwhite,ncol = 2, nrow = 2)
+    ggsave("Figures/CovBalanceQuad.pdf", height = 6, width = 9)
+    
+"------------------------------RDD Estimation on Recidivism------------------------------"
+
+# Model (1): Y = DUI + bac1 + bac1*DUI + X
+dt <- dt %>% mutate(bac1_centDUI2=(bac1_cent*DUI)^2)
+#Models
+  mdl1 <- recidivism ~ DUI + bac1_cent + white + male + aged + acc
+  mdl2 <- recidivism ~ DUI + bac1_cent + bac1_cent*DUI + white + male + aged + acc
+  mdl3 <- recidivism ~ DUI + bac1_cent + bac1_cent*DUI + bac1_centDUI2 + white + male + aged + acc
+
+#Panel A: Bandwidth h = 0.05
+  mdl_a1 <- lm(formula = mdl1, data = dt, weights = kernel_r)
+  mdl_a1_r <- coeftest(mdl_a1, vcov = vcovHC(mdl_a1,"HC1"))
   
+  mdl_a2 <- lm(formula = mdl2, data = dt, weights = kernel_r)
+  mdl_a2_r <- coeftest(mdl_a2, vcov = vcovHC(mdl_a2,"HC1"))
+
+  mdl_a3 <- lm(formula = mdl3, data = dt, weights = kernel_r)
+  mdl_a3_r <- coeftest(mdl_a3, vcov = vcovHC(mdl_a3,"HC1"))
   
+  #Generate table A
+  stargazer(mdl_a1_r,mdl_a2_r,mdl_a3_r, title = "", 
+            out = "Tables/RDresultsA.tex", 
+            out.header = T,
+            header = F,
+            column.labels = c("Linear Control","Interaction","Squared Interaction"),
+            covariate.labels = "DUI",
+            keep = "DUI", nobs = T, mean.sd = T, label = "RDresultsA")
   
+#Panel B: Bandwidth h = 0.025
+  #New Rectangular Kernel
+  h = 0.025
+  dt <- dt %>% mutate(kernel_r2=ifelse(abs(bac1_cent)<h,1/(2*h),0))
   
-  ##creating a fake dataset (N=1000, 500 at treated, 500 at control group)
-  #outcome variable
-  outcome <- c(rnorm(500, mean = 50, sd = 10),  rnorm(500, mean = 70, sd = 10))
+  mdl_b1 <- lm(formula = mdl1, data = dt, weights = kernel_r2)
+  mdl_b1_r <- coeftest(mdl_b1, vcov = vcovHC(mdl_b1,"HC1"))
   
-  #running variable
-  running.var <- seq(0, 1, by = .0001)
-  running.var <- sample(running.var, size = 1000, replace = T)
+  mdl_b2 <- lm(formula = mdl2, data = dt, weights = kernel_r2)
+  mdl_b2_r <- coeftest(mdl_b2, vcov = vcovHC(mdl_b2,"HC1"))
   
-  ##Put negative values for the running variable in the control group
-  running.var[1:500] <- -running.var[1:500]
+  mdl_b3 <- lm(formula = mdl3, data = dt, weights = kernel_r2)
+  mdl_b3_r <- coeftest(mdl_b3, vcov = vcovHC(mdl_b3,"HC1"))  
   
-  #treatment indicator (just a binary variable indicating treated and control groups)
-  treat.ind <- c(rep(0,500), rep(1,500))
-  data <- data.frame(cbind(outcome, running.var))
-                           
-   ggplot(data, aes(running.var, outcome, color = treat.ind, )) +
-     geom_point() + geom_smooth(method=loess) +
-     geom_vline(xintercept=0, linetype="longdash") +
-     xlab("Running variable") +
-     ylab("Outcome variable") +
-     theme_classic()
-
-
-
-
-
-
-
-
-
-
+  #Generate table B
+  stargazer(mdl_b1_r,mdl_b2_r,mdl_b3_r, title = "", 
+            out = "Tables/RDresultsB.tex", 
+            out.header = T,
+            header = F,
+            column.labels = c("Linear Control","Interaction","Squared Interaction"),
+            covariate.labels = "DUI",
+            keep = "DUI", nobs = T, mean.sd = T, label = "RDresultsB")
   
+  "------------------------------RDD Estimation on Recidivism Graphs------------------------------"
+  
+  #Filter Data on BAC<0.15
+  dt <- dt %>% filter(bac1<0.15)
+  #Update bins
+  bin <- cut(dt$bac1, seq(min(dt$bac1),max(dt$bac1),0.002)) #bins = 0.002 as in the paper
+  data_bin <- aggregate(dt,list(bin),function(x) { return(c(mean(x),length(x)))})
+  
+  #Graphs: BAC on Recidivism 
+  #Linear Fit
+  ggrecidl <- ggplot() + 
+    geom_point(data = data_bin, aes(x=bac1[,1],y=recidivism[,1], alpha=.5)) + 
+    stat_smooth(data = dt, aes(x = bac1, y = recidivism, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm)+
+    geom_vline(xintercept = 0.08, linetype = "longdash")+
+    scale_x_continuous(name = "BAC")+
+    scale_y_continuous(name = "")+
+    scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+    scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control L. Fit","Treatment L. Fit"))+
+    labs(title = "BAC on Recidivism", subtitle = "Linear Fit")+
+    theme_classic()+
+    theme(legend.position = "bottom")
+  ggrecidl  
+  
+  #Quadratic Fit
+  ggrecidq <- ggplot() + 
+    geom_point(data = data_bin, aes(x=bac1[,1],y=recidivism[,1], alpha=.5)) + 
+    stat_smooth(data = dt, aes(x = bac1, y = recidivism, color = factor(DUI), group = factor(DUI)), size = 0.5, method = lm, formula = y ~ poly(x,2))+
+    geom_vline(xintercept = 0.08, linetype = "longdash")+
+    scale_x_continuous(name = "BAC")+
+    scale_y_continuous(name = "")+
+    scale_alpha_continuous(name = "BAC", breaks = "0.5", labels = "")+
+    scale_color_manual(values = c("royalblue4","dodgerblue"), name = "", breaks = c("0","1"), labels = c("Control Q. Fit","Treatment Q. Fit"))+
+    labs(title = "BAC on Recidivism", subtitle = "Quadratic Fit")+
+    theme_classic()+
+    theme(legend.position = "bottom")
+  ggrecidq 
+  
+  #Arrange the plots
+  ggarrange(ggrecidl,ggrecidq,ncol = 2, nrow = 1)
+  ggsave("Figures/RDDrecid.pdf", height = 4, width = 9)
+    
